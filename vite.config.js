db8968f4b26cbe94aebdcc9f1d1163812d06e939
Alex <alexnowik07@gmail.com>
2026-06-16 15:51:18 +0300

    Fix deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        presentation: 'presentation.html',
+        presentation_light: 'presentation_light.html',
+      },
+    },
+  },
 });
